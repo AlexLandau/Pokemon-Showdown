05f87445d4a5c17feccd7a5ec86cf3218a61ca30
AlexLandau <alandau@cs.stanford.edu>
2019-09-21 13:01:50 -0700

    Add a decent approach to collecting stats

--- sim/tools/pickrandom.ts
+++ sim/tools/pickrandom.ts
@@ -1,8 +1,8 @@
 import { Dex, ModdedDex } from "../dex";
 import { Battle } from "../battle";
+import * as fs from "fs";
 
-// const Dex = require('../../.sim-dist/dex').Dex;
-// Dex.includeModData();
+// TODO: Case to look into: Venusaur with Solarbeam losing to Jigglypuff with Disable
 
 const md: ModdedDex = Dex.mod("gen1");
 const pd = md.data.Pokedex;
@@ -14,7 +14,7 @@ const pd = md.data.Pokedex;
 // console.log("bulba: " + Object.keys(pd.bulbasaur));
 // Dex.forFormat("gen1customgame");
 
-const somePokemonNames = Object.keys(pd).slice(1, 152);
+const gen1PokemonNames = Object.keys(pd).slice(1, 152);
 
 // console.log(JSON.stringify(somePokemonNames));
 // console.log(somePokemonNames.length);
@@ -27,15 +27,32 @@ function getRandomInt(min: number, max: number) {
 
 function pickRandomPokemon(): string {
 	const pokemonIndex = getRandomInt(0, 151);
-	const pokemonName = somePokemonNames[pokemonIndex];
+	const pokemonName = gen1PokemonNames[pokemonIndex];
 	return pokemonName;
 }
 
 function pickRandomMove(pokemonName: string): string {
-	const learnset = md.data.Learnsets[pokemonName].learnset;
-	const eligibleMoves = Object.keys(learnset).filter(moveName => learnset[moveName].find(moveSource => moveSource.startsWith("1")) != undefined);
+	const eligibleMoves = getLegalMovesFor(pokemonName);
 	const randomMove = eligibleMoves[getRandomInt(0, eligibleMoves.length)];
 	return randomMove;
+}
+
+function getLegalMovesFor(pokemonName: string): string[] {
+	const learnset = md.data.Learnsets[pokemonName].learnset;
+	const moves: Set<string> = new Set();
+	for (const moveName in learnset) {
+		if (learnset[moveName].find(moveSource => moveSource.startsWith("1")) != undefined) {
+			moves.add(moveName);
+		}
+	}
+	// Check for moves only learnable before evolving, which are not in the learnset
+	const prevo = md.getTemplate(pokemonName).prevo;
+	if (prevo) {
+		for (const prevoMove of getLegalMovesFor(prevo)) {
+			moves.add(prevoMove);
+		}
+	}
+	return Array.from(moves).sort();
 }
 
 // const pokemonIndex = getRandomInt(0, 151);
@@ -57,18 +74,20 @@ function pickRandomMove(pokemonName: string): string {
 
 // Next step is to figure out how to pit a couple of Pokemon together in a battle...
 const perfectEvs: StatsTable = {hp: 255, atk: 255, def: 255, spe: 255, spa: 255, spd: 255};
-const perfectIvs: StatsTable = {hp: 31, atk: 31, def: 31, spe: 31, spa: 31, spd: 31};
+// 30 is perfect for gen 1, 31 for later gens
+const perfectIvs: StatsTable = {hp: 30, atk: 30, def: 30, spe: 30, spa: 30, spd: 30};
 
 function getBattleWinner(pset1: PokemonSet, pset2: PokemonSet): string | undefined {
 
 	const battle = new Battle({
 		formatid: Dex.getId("gen1custombattle"),
-		p1: {name: "P1", team: [pset1]},
-		p2: {name: "P2", team: [pset2]},
+		p1: {name: "p1", team: [pset1]},
+		p2: {name: "p2", team: [pset2]},
 		strictChoices: true
 	});
 
 	battle.start();
+
 	while (!battle.ended) {
 		// battle.makeChoices(`move ${move1}`, `move ${move2}`);
 		// console.log(`${battle.sides[0].pokemon[0].getHealth().secret} vs. ${battle.sides[1].pokemon[0].getHealth().secret}`);
@@ -87,7 +106,65 @@ function getBattleWinner(pset1: PokemonSet, pset2: PokemonSet): string | undefin
 	return winner;
 }
 
-// Case to look into: Venusaur with Solarbeam losing to Jigglypuff with Disable
+function getBattleWinner2(pokemon1: string, move1: string, pokemon2: string, move2: string): "p1" | "p2" | "dnf" {
+	const pset1: PokemonSet = {
+		name: pokemon1,
+		species: pokemon1,
+		item: "",
+		ability: "",
+		moves: [move1],
+		nature: "",
+		gender: "",
+		evs: perfectEvs,
+		ivs: perfectIvs,
+		level: 100
+	};
+
+	const pset2: PokemonSet = {
+		name: pokemon2,
+		species: pokemon2,
+		item: "",
+		ability: "",
+		moves: [move2],
+		nature: "",
+		gender: "",
+		evs: perfectEvs,
+		ivs: perfectIvs,
+		level: 100
+	};
+
+	const battle = new Battle({
+		formatid: Dex.getId("gen1custombattle"),
+		p1: {name: "p1", team: [pset1]},
+		p2: {name: "p2", team: [pset2]},
+		strictChoices: true
+	});
+
+	battle.start();
+
+	while (!battle.ended) {
+		// battle.makeChoices(`move ${move1}`, `move ${move2}`);
+		// console.log(`${battle.sides[0].pokemon[0].getHealth().secret} vs. ${battle.sides[1].pokemon[0].getHealth().secret}`);
+		battle.makeChoices();
+		// battle.choose();
+		// console.log(`Turn: ${battle.turn}`);
+		if (battle.turn === 200) {
+			break;
+		}
+	}
+	const winner = battle.winner;
+
+	console.log(`${winner} won after ${battle.turn} turns`);
+	// Eventually...
+	battle.destroy();
+	if (winner === "p1" || winner === "p2") {
+		return winner;
+	} else if (winner === undefined) {
+		return "dnf";
+	}
+	throw new Error(`Unexpected winner value ${winner}`);
+}
+
 function runRandomBattle() {
 	const p1 = pickRandomPokemon();
 	const move1 = pickRandomMove(p1);
@@ -132,6 +209,88 @@ function runRandomBattle() {
 
 }
 
-for (let i = 0; i < 10; i++) {
-	runRandomBattle();
+const TARGET_RUNS_PER_COMBINATION = 10;
+
+function collectBattleData() {
+	for (let pi1 = 0; pi1 < gen1PokemonNames.length; pi1++) {
+		const pokemon1 = gen1PokemonNames[pi1];
+		const moves1 = getLegalMovesFor(pokemon1);
+		for (let mi1 = 0; mi1 < moves1.length; mi1++) {
+			const move1 = moves1[mi1];
+			const winCountsByOpponent = loadExistingWinCountsByOpponent(pokemon1, move1);
+			for (let pi2 = pi1; pi2 < gen1PokemonNames.length; pi2++) {
+				const pokemon2 = gen1PokemonNames[pi2];
+				const moves2 = getLegalMovesFor(pokemon2);
+				const movesStartingPoint = pi1 === pi2 ? mi1 + 1 : 0;
+				for (let mi2 = movesStartingPoint; mi2 < moves2.length; mi2++) {
+					const move2 = moves2[mi2];
+
+					const oppId = pokemon2 + " " + move2;
+					const winCounts: WinCounts = oppId in winCountsByOpponent ? winCountsByOpponent[oppId] : {p1: 0, p2: 0, dnf: 0};
+					const alreadyRunCount = winCounts.p1 + winCounts.p2 + winCounts.dnf;
+					if (alreadyRunCount >= TARGET_RUNS_PER_COMBINATION) {
+						continue;
+					}
+					console.log(`${pokemon1} with ${move1} (PP ${Dex.getMove(move1).pp}) vs. ${pokemon2} with ${move2} (PP ${Dex.getMove(move2).pp})`);
+
+					for (let i = alreadyRunCount; i < TARGET_RUNS_PER_COMBINATION; i++) {
+						const winner = getBattleWinner2(pokemon1, move1, pokemon2, move2);
+						if (winner != undefined) {
+							winCounts[winner as "p1" | "p2"]++;
+						}
+					}
+					console.log(`Win counts: ${JSON.stringify(winCounts)}`);
+					winCountsByOpponent[oppId] = winCounts;
+				}
+				// console.log(`So far: ${JSON.stringify(winCountsByOpponent)}`);
+				console.log("Saving...");
+				writeToFile(pokemon1, move1, winCountsByOpponent);
+			}
+			// TODO: Write winCountsByOpponent to a file
+		}
+	}
 }
+
+type WinCounts = { p1: number, p2: number, dnf: number };
+type WinCountsByOpp = { [opponent: string]: WinCounts };
+
+function loadExistingWinCountsByOpponent(pokemon: string, move: string): WinCountsByOpp {
+	const path = `collectedStats/${pokemon}_${move}`;
+	if (!fs.existsSync(path)) {
+		return {};
+	}
+	const fileText = fs.readFileSync(path, { encoding: "utf8" });
+	const wcbo: WinCountsByOpp = {};
+	for (const line of fileText.split("\n")) {
+		const lineParts = line.split(" ");
+		if (lineParts.length === 5) {
+			const oppPokemon = lineParts[0];
+			const oppMove = lineParts[1];
+			const p1 = parseInt(lineParts[2]);
+			const p2 = parseInt(lineParts[3]);
+			const dnf = parseInt(lineParts[4]);
+
+			wcbo[oppPokemon + " " + oppMove] = { p1, p2, dnf };
+		} else if (lineParts.length > 1) {
+			console.log("Invalid line in file " + path);
+		}
+	}
+	return wcbo;
+}
+
+function writeToFile(pokemon: string, move: string, winCountsByOpponent: WinCountsByOpp) {
+	if (!fs.existsSync("collectedStats")) {
+		fs.mkdirSync("collectedStats");
+	}
+	const path = `collectedStats/${pokemon}_${move}`;
+	const contentLines: string[] = [];
+	for (const opponent in winCountsByOpponent) {
+		const winCounts = winCountsByOpponent[opponent];
+		const line = `${opponent} ${winCounts.p1} ${winCounts.p2} ${winCounts.dnf}`;
+		contentLines.push(line);
+	}
+	const contentText = contentLines.join("\n");
+	fs.writeFileSync(path, contentText);
+}
+
+collectBattleData();
